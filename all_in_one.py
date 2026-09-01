e0eeaf111695989c8606d1f099d85844c7de7d1c
abhishek-jatav-pro <senimafilm@gmail.com>
2026-09-01 19:45:35 +0530

    Update all_in_one.py

--- all_in_one.py
+++ all_in_one.py
@@ -1,35 +1,43 @@
-# ABHISHEK ALL-IN-ONE PRO v5.1
+# ABHISHEK ALL-IN-ONE SUPER PRO v6.0
 # Made by Abhishek Jatav
 
-print("=== ABHISHEK ALL-IN-ONE PRO ===")
+import random
+import datetime
+
+print("=== ABHISHEK ALL-IN-ONE SUPER PRO v6.0 ===")
 
 while True:
     print("\n1. Calculator")
     print("2. Age Calculator")
-    print("3. File Save Test")
-    print("4. Exit")
+    print("3. Password Generator")
+    print("4. To-Do List Save")
+    print("5. Exit")
 
-    ch = input("Choice daalo (1-4): ")
+    ch = input("Choice (1-5): ")
 
     if ch == '1':
         a = int(input("Pehla number: "))
         b = int(input("Dusra number: "))
-        print(f"Jod = {a+b}")
+        print(f"Jod: {a+b} | Guna: {a*b}")
         with open("data.txt", "a") as f:
-            f.write(f"Calc: {a}+{b}={a+b}\n")
+            f.write(f"Calc: {a},{b}\n")
 
     elif ch == '2':
         birth = int(input("Birth Year: "))
-        print(f"Tumhari Age: {2026 - birth}")
+        print(f"Age: {2026 - birth} saal")
 
     elif ch == '3':
-        name = input("Apna naam likho: ")
-        with open("data.txt", "a") as f:
-            f.write(f"User: {name}\n")
-        print("File me save ho gaya!")
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$"
+        length = int(input("Password length?: "))
+        pwd = "".join(random.choice(chars) for _ in range(length))
+        print(f"Password: {pwd}")
 
     elif ch == '4':
-        print("Bye Bhai! Project Khatam!")
+        task = input("Kaam likho: ")
+        with open("data.txt", "a") as f:
+            f.write(f"TODO: {task}\n")
+        print("Save Ho Gaya!")
+
+    elif ch == '5':
+        print("Bye! v6.0 Khatam!")
         break
-    else:
-        print("Galat choice!")v
